b03f78c50ea3d6c8bc3ad801b01b8769df295b89
MuniWobbler <22512238+MuniWobbler@users.noreply.github.com>
2020-08-18 19:43:16 +1200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 let strip = neopixel.create(DigitalPin.P0, 10, NeoPixelMode.RGB)
 strip.showRainbow(1, 360)
 basic.forever(function () {
-    if (0 < 0) {
+    if (input.acceleration(Dimension.X) < 0) {
         strip.rotate(1)
     } else {
         strip.rotate(-1)
